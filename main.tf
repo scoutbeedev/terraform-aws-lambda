75ebd7e6dd8736571c7598d84c25fe533d05b701
Aditya Menon <aditya.menon@scoutbee.com>
2023-06-22 10:29:57 +0200

    [N/A] Ignore S3 key changes

--- main.tf
+++ main.tf
@@ -136,6 +136,12 @@ resource "aws_lambda_function" "this" {
     aws_iam_role_policy_attachment.vpc,
     aws_iam_role_policy_attachment.tracing,
   ]
+  lifecycle {
+    ignore_changes = [
+      s3_key,
+      s3_object_version
+    ]
+  }
 }
 
 resource "aws_lambda_layer_version" "this" {
